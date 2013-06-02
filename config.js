2d2d5bbfe907723afa15e98e9c571787ba81ae3e
Olof Olsson <olof.d04oo@gmail.com>
2013-06-02 13:34:48 +0200

    Cleared the config file

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	dev:{
 		fb:{
-			appId: '549613371744963',
-			appSecret: '409f5c4e845285adf40fe0a38e65f620',
+			appId: '',
+			appSecret: '',
 			url: 'http://kaizen.menmo.se/'
 		},
 		dbUrl: 'mongodb://localhost/kaizen'
